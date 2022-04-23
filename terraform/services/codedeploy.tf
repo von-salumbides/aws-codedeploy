4d6ddb7428e7377db692c273b1e62d2a27eec289
von-salumbides <vonsalumbides.devops@gmail.com>
2022-04-23 09:37:25 +0800

    AWS: codedeploy

--- terraform/services/codedeploy.tf
+++ terraform/services/codedeploy.tf
@@ -5,3 +5,12 @@ module "codedeploy" {
   # compute_platform = "" // isDefined ? var.compute_platform : default==Server
 }
 
+module "codedeploy-group" {
+  source       = "git::https://github.com/von-salumbides/terraform-module.git//aws-codedeploy-deploymentgroup-ec2"
+  env          = var.env
+  project      = var.project
+  purpose      = "ec2"
+  config       = "ec2_tag_filter"
+  iam-role-ec2 = module.iam-role-ec2.id
+  app_name     = module.codedeploy.name
+}
